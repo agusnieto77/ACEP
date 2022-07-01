9502076133e1a1a9d6c4d77cf5cad34d734688c2
agusnieto77 <agustin.nieto77@gmail.com>
2022-07-01 15:48:39 -0300

    up

--- R/acep_diccionarios.R
+++ R/acep_diccionarios.R
@@ -1,9 +1,12 @@
 #' @title Colección de diccionarios.
-#' @description Colección de diccionarios que reúne diccionarios de diferentes orígenes. Los diccionarios dicc_confl_gp y dicc_viol_gp fueron extraidos de Albrieu y Palazzo (2020).
+#' @description Colección de diccionarios que reúne diccionarios de diferentes orígenes. El diccionario dicc_confl_acep fueron construidos en el marco del Observatorio de Conflictividad de la UNMdP. Los diccionarios dicc_confl_gp y dicc_viol_gp fueron extraidos de Albrieu y Palazzo (2020).
 #' @examples
+#' dicc_confl_acep <- acep_diccionarios$dicc_confl_sismos
 #' dicc_confl_gp <- acep_diccionarios$dicc_confl_gp
 #' dicc_viol_gp <- acep_diccionarios$dicc_viol_gp
 #' @references
-#' Albrieu, Ramiro, y Gabriel Palazzo 2020 «Categorización de conflictos sociales en el ámbito de los recursos naturales: un estudio de las actividades extractivas mediante la minería de textos». Revista CEPAL (131):29-59.
+#' Albrieu, Ramiro y Gabriel Palazzo 2020 «Categorización de conflictos sociales en el ámbito de los recursos naturales: un estudio de las actividades extractivas mediante la minería de textos». Revista CEPAL (131):29-59.
+#' Laitano, Guillermina y Agustín Nieto «Análisis computacional de la conflictividad laboral en Mar del Plata durante el gobierno de Cambiemos». Ponencia presentado en VI Workshop - Los conflictos laborales en la Argentina del siglo XX y XXI: un abordaje interdisciplinario de conceptos, problemas y escalas de análisis, Tandil, 2021.
+
 
 "acep_diccionarios"
