62578a93384fc0a0665fe5ec38a3bbaddeae17d1
agusnieto77 <agustin.nieto77@gmail.com>
2022-10-23 22:29:03 -0300

    Actualización

--- R/acep_token.R
+++ R/acep_token.R
@@ -12,14 +12,18 @@
 acep_token <- function(x, tolower = TRUE) {
   if(tolower == TRUE){
     id = data.frame(id_doc = 1:length(x), texto = x)
-    id_token = 1: length(unlist(strsplit(tolower(x), " ")))
+    id_token = 1:length(unlist(strsplit(tolower(x), " ")))
     token <- unlist(strsplit(tolower(x), " "))
     texto <- rep(x,sapply(strsplit(x, ' '), length))
   } else {
+    id = data.frame(id_doc = 1:length(x), texto = x)
+    id_token = 1:length(unlist(strsplit(x, " ")))
     token <- unlist(strsplit(x, " "))
     texto <- rep(x,sapply(strsplit(x, ' '), length))
   }
   df <- merge(data.frame(texto = texto, id_token = id_token,token = token),id)
-  df <- data.frame(id_doc = df$id_doc, texto = df$texto, id_token = df$id_token, token = df$token)
-  return(df[order(df$id_token, decreasing = FALSE),])
+  df <- df[order(df$id_token, decreasing = FALSE),]
+  df$id_token_doc <-unlist(aggregate(df$token ~ df$id_doc, FUN=function(x) 1:length(x))[,2])
+  df <- data.frame(id_doc = df$id_doc, texto = df$texto, id_token = df$id_token, id_token_doc = df$id_token_doc, token = df$token)
+  return(df)
 }
