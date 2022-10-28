c521745531cd5a6c12e1441db925b6f9d66acfe3
agusnieto77 <agustin.nieto77@gmail.com>
2022-10-28 15:00:12 -0300

    Actualización

--- tests/testthat/test-acep_token.R
+++ tests/testthat/test-acep_token.R
@@ -3,7 +3,7 @@ test_that("ACEP TOKEN", {
   skip_if_offline()
   skip_on_cran()
   rev_puerto <- acep_bases$rev_puerto[1:10,]
-  rev_puerto_token <- acep_token(rev_puerto$nota)
+  rev_puerto_token <- acep_token(rev_puerto$titulo)
   dimensiones <- length(rev_puerto_token$token)
   expect_equal(dimensiones, length(rev_puerto_token$id_token))
 })
@@ -12,7 +12,7 @@ test_that("ACEP TOKEN F", {
   skip_if_offline()
   skip_on_cran()
   rev_puerto <- acep_bases$rev_puerto[1:10,]
-  rev_puerto_token <- acep_token(rev_puerto$nota, tolower = FALSE)
+  rev_puerto_token <- acep_token(rev_puerto$titulo, tolower = FALSE)
   dimensiones <- length(rev_puerto_token$token)
   expect_equal(dimensiones, length(rev_puerto_token$id_token))
 })
