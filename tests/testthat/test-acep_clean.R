941904902441981ecc096a5e228630a15c2f3652
agusnieto77 <agustin.nieto77@gmail.com>
2022-10-28 14:42:22 -0300

    Actualización

--- tests/testthat/test-acep_clean.R
+++ tests/testthat/test-acep_clean.R
@@ -2,143 +2,143 @@
 test_that("ACEP CLEAN", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN TF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, tolower = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, tolower = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMCF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_cesp = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_cesp = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMEF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_emoji = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_emoji = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMHF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_hashtag = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_hashtag = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMUF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_users = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_users = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMPF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_punt = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_punt = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMNF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_num = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_num = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMURLF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_url = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_url = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMMF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_meses = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_meses = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMDF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_dias = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_dias = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMSTWF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_stopwords = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_stopwords = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMSTWTN", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, other_sw = c('mar','plata','puerto'))
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, other_sw = c('mar','plata','puerto'))
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMSHWF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_shortwords = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_shortwords = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMNLF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_newline = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_newline = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
 
 test_that("ACEP CLEAN RMWSF", {
   skip_if_offline()
   skip_on_cran()
-  rev_puerto <- acep_bases$rev_puerto
-  rev_puerto$nota_clean <- acep_clean(rev_puerto$nota, rm_whitespace = FALSE)
-  dimensiones <- length(rev_puerto$nota)
+  rev_puerto <- acep_bases$rev_puerto[1:10, ]
+  rev_puerto$titulo_clean <- acep_clean(rev_puerto$titulo, rm_whitespace = FALSE)
+  dimensiones <- length(rev_puerto$titulo)
   expect_equal(dimensiones, length(rev_puerto$fecha))
 })
--- tests/testthat/test-acep_load_base.R
+++ tests/testthat/test-acep_load_base.R
@@ -2,10 +2,8 @@
 test_that("ACEP LOAD", {
   skip_if_offline()
   skip_on_cran()
-  tag <- 'https://zenodo.org/record/6835713/files/bd_sismos_mdp.rds?download=1'
+  tag <- 'https://github.com/HDyCSC/datos/raw/67100a9b73c3fc9053c9dc9227a113bc2d317dae/bd_sismos_mdp.rds'
   oc <- acep_load_base(tag = tag)
   dimensiones <- length(oc$id)
   expect_equal(dimensiones, 5132)
 })
-
-
