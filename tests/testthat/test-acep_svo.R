5e8e723deb309f409c32fe59e8ed0fd8e432afb7
agusnieto77 <agustin.nieto77@gmail.com>
2022-11-24 10:34:35 -0300

    Actualización

--- tests/testthat/test-acep_svo.R
+++ tests/testthat/test-acep_svo.R
@@ -2,7 +2,7 @@
 test_that("ACEP svo", {
   skip_if_offline()
   skip_on_cran()
-  texto <- acep_postag("El SUTEBA fue al paro por mejoras salariales.")
+  texto <- acep_postag("El SUTEBA fue al paro por mejoras salariales.", bajar_core = FALSE)
   acep_svo(texto$texto_tag)
   dimensiones <- length(texto)
   expect_equal(dimensiones, length(texto))
