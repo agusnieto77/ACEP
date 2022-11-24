d07bdeb3bce0247806c93aa520917f427c9fa67b
agusnieto77 <agustin.nieto77@gmail.com>
2022-11-24 11:17:08 -0300

    Actualización

--- tests/testthat/test-acep_postag.R
+++ tests/testthat/test-acep_postag.R
@@ -2,7 +2,7 @@
 test_that("ACEP postag", {
   skip_if_offline()
   skip_on_cran()
-  texto <- acep_postag("SUTEBA went on strike for better salaries.", bajar_core = FALSE, core = "en_core_web_sm")
+  texto <- acep_postag("SUTEBA went on strike for better salaries.", core = "en_core_web_sm")
   dimensiones <- length(texto)
   expect_equal(dimensiones, length(texto))
 })
--- tests/testthat/test-acep_svo.R
+++ tests/testthat/test-acep_svo.R
@@ -2,7 +2,7 @@
 test_that("ACEP svo", {
   skip_if_offline()
   skip_on_cran()
-  texto <- acep_postag("SUTEBA went on strike for better salaries.", bajar_core = FALSE, core = "en_core_web_sm")
+  texto <- acep_postag("SUTEBA went on strike for better salaries.", core = "en_core_web_sm")
   acep_svo(texto$texto_tag)
   dimensiones <- length(texto)
   expect_equal(dimensiones, length(texto))
