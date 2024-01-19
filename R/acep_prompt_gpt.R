f768d8ad5ea7010f5366bb801c1699d36fad8220
agusnieto77 <agustin.nieto77@gmail.com>
2024-01-18 23:10:56 -0300

    Actualización

--- R/acep_prompt_gpt.R
+++ R/acep_prompt_gpt.R
@@ -3,10 +3,22 @@
 #' de OpenAI. Las instrucciones fueron testeadas en el marco de las tareas
 #' que realizamos en el Observatorio de Conflictividad Social de la
 #' Universidad Nacional de Mar del Plata.
-#' @format Es un objeto de clase 'list' con 1 componente.
+#' @format Es un objeto de clase 'list' con 4 componentes.
 #'\describe{
-#' \item{dicc_confl_gp}{es un texto con instrucciones para que el modelo
-#'  gpt-3 turbo de OpenAI devuelva una codificación de eventos de protesta.}
+#' \item{instruccion_breve_sao_es}{es un texto en castellano con instrucciones breves para
+#'  extraer eventos de protesta y codificarlos con las siguientes claves: 'fecha',
+#'  'sujeto', 'accion', 'objeto', 'lugar'.}
+#'  \item{instruccion_larga_sao_es}{es un texto en castellano con instrucciones largas para
+#'  extraer eventos de protesta y codificarlos con las siguientes claves: 'id',
+#'  'cronica', 'fecha', 'sujeto', 'organizacion', 'participacion', 'accion',
+#'  'objeto', 'lugar'.}
+#' \item{instruccion_breve_sao_en}{es un texto en inglés con instrucciones breves para
+#'  extraer eventos de protesta y codificarlos con las siguientes claves: 'date',
+#'  'subject', 'action', 'object', 'place'.}
+#'  \item{instruccion_larga_sao_en}{es un texto en inglés con instrucciones largas para
+#'  extraer eventos de protesta y codificarlos con las siguientes claves: 'id',
+#'  'chronicle', 'date', 'subject', 'organization', 'participation', 'action',
+#'  'object', 'place'.}
 #'}
 #' @docType data
 #' @usage data(acep_prompt_gpt)
